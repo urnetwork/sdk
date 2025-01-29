958445fa4e9bbbd1e16a3d072d12c9d9d2f707d9
Brien Colwell <xcolwell@gmail.com>
2025-01-29 04:30:03 -0800

    perf changes

--- device.go
+++ device.go
@@ -224,7 +224,7 @@ type deviceLocalSettings struct {
 
 func defaultDeviceLocalSettings() *deviceLocalSettings {
 	return &deviceLocalSettings{
-		SendTimeout: 5 * time.Second,
+		SendTimeout: 1 * time.Second,
 		// ClientDrainTimeout: 30 * time.Second,
 
 		NetContractStatusDuration: 10 * time.Second,
