0f5213ca55c9e3ffa81e7a24c11d530a374e7d16
Product Builder <builder@bringyour.com>
2026-01-06 23:48:42 -0800

    2026.1.6-829720580

--- v2026/device_local.go
+++ v2026/device_local.go
@@ -18,10 +18,10 @@ import (
 
 	gojwt "github.com/golang-jwt/jwt/v5"
 
-	"github.com/urnetwork/glog"
+	"github.com/urnetwork/glog/v2026"
 
-	"github.com/urnetwork/connect"
-	"github.com/urnetwork/connect/protocol"
+	"github.com/urnetwork/connect/v2026"
+	"github.com/urnetwork/connect/v2026/protocol"
 )
 
 type emptyWindowMonitor struct {
